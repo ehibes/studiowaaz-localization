0a4c717b7ebd9a336c10776d47575a814ed209ee
ehibes <ibes.mongabure@gmail.com>
2017-10-23 17:10:27 +0200

    doc & pieces translatable; @TODO areas in pieces; doc

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-var async = require('async');
-var _ = require('lodash');
-
 module.exports = {
 
   moogBundle: {
-    modules: ['extend-page-cursors'],
+    modules: ['extend-page-cursors','studiowaaz-docs', 'studiowaaz-pieces'],
     directory: 'lib/modules'
   },
 
   construct: function(self, options) {
     self.defaultLocale = options.default || "en";
     self.locales = options.locales;
-    self.neverTypes =options.neverTypes || ['apostrophe-images'];
+    self.neverTypes = options.neverTypes || ['apostrophe-images'];
     if (options.prefixes !== undefined) {
       self.prefixes = options.prefixes;
     } else {
       self.prefixes = true;
     }
-    console.log('root init');
 
     require('./lib/middleware.js')(self, options);
-    require('./lib/localization.js')(self, options); 
   }
 
 };
