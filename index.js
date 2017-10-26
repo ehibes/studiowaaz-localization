147ad41d165bb76a769761150dc1eb164cdc9af1
ehibes <ibes.mongabure@gmail.com>
2017-10-26 23:04:09 +0200

    lodash merge use - new helper - _localeurl definition on cursors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 module.exports = {
 
   moogBundle: {
-    modules: ['extend-page-cursors','studiowaaz-docs', 'studiowaaz-pieces'],
+    modules: ['extend-page-cursors','studiowaaz-docs', 'studiowaaz-pieces', 'studiowaaz-pieces-pages'],
     directory: 'lib/modules'
   },
+
+  alias: 'waaz',
 
   construct: function(self, options) {
     self.defaultLocale = options.default || "en";
@@ -16,6 +18,23 @@ module.exports = {
     }
 
     require('./lib/middleware.js')(self, options);
+    self.addHelpers({
+      // localePicker:function(args) {
+      //   var locales = {};
+      //   _.each(self.locales, function(value) {
+      //     locales[value] = '/'+value+self.apos.templates.contextReq.url;
+      //   });
+      //   return self.partial('localePicker', {locales: locales, current: self.apos.templates.contextReq.data.activeLocale});
+
+      // },
+      // toLocalUrl: function(url) {
+      //   // Replace spaces with claps
+      //   return "/"+ self.apos.templates.contextReq.data.activeLocale+ url;
+      // },
+      getLocale: function() {
+        return self.apos.templates.contextReq.data.activeLocale;
+      }
+    });
   }
 
 };
